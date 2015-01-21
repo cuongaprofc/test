183d918f357ab5cb5b8135d0b0094ef88f26271f
cuongaprofc <cuong.aprofc@gmail.com>
2015-01-21 11:43:28 +0700

    create new project

---
+++ test-github/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace test_github
+{
+    static class Program
+    {
+        //create new project
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
